38c295c8ab47404306701b8295e7fd8dfb7765c5
Filip Taik <filiptaik@users.noreply.github.com>
2019-09-10 11:39:55 +0300

    lisatud aja muutmine

--- 02_tund/testpage.php
+++ 02_tund/testpage.php
@@ -1,5 +1,14 @@
 <?php
 	$userName = "Filip Taik";
+	$fullTimeNow = date("d.m.Y H:i:s");
+	$hourNow = date("H");
+	$partOfDay = "hägune aeg";
+	
+	if($hourNow < 8){
+		$partOfDay = "hommik";
+	}
+	
+	
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -16,5 +25,11 @@
   echo "<h1>" . $userName . " ei salli neid haigeid putukaid" . "</h1>";
   ?>
   <p>see veebileht on loodud õppetöö käigus ega sisalda mingit adekvaatset sisu</p>
+  <hr>
+  <?php
+  echo "<p>Lehe avamise hetkel oli aeg: " . $fullTimeNow . ", " . $partOfDay . ".</p>" ; 
+  ?>
+  <br>
+  
 </body>
 </html>
