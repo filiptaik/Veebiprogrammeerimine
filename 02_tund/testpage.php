a8c77f3e3b21f0dce58e02677056368d263aada2
Filip Taik <filiptaik@users.noreply.github.com>
2019-09-10 11:09:14 +0300

    esimene üleslaadimine

---
+++ 02_tund/testpage.php
@@ -0,0 +1,20 @@
+<?php
+	$userName = "Filip Taik";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $userName;
+  ?>
+   on saatanast</title>
+</head>
+<body>
+  <?php
+  echo "<h1>" . $userName . " ei salli neid haigeid putukaid" . "</h1>";
+  ?>
+  <p>see veebileht on loodud õppetöö käigus ega sisalda mingit adekvaatset sisu</p>
+</body>
+</html>
